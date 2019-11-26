db10096cb6379af6b736e07fbb21aa7b90240051
Vlad Matveev <49278415+decentos@users.noreply.github.com>
2019-11-26 19:13:20 +0300

    [+] Start game

---
+++ TicTacToe/GraphicsTicTacToe/GameStart.java
@@ -0,0 +1,12 @@
+package GraphicsTicTacToe;
+
+import GraphicsTicTacToe.Logic.Game;
+
+public class GameStart {
+    public static void main(String[] args) {
+
+        Game gameInstance = new Game();
+        gameInstance.initGame();
+
+    }
+}
